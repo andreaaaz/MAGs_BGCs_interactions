ae1815135f6c25e1e51259fc5fbce9325c33ea9e
andreaaaz <andyszermeno@gmail.com>
2025-05-01 03:43:22 -0600

    parameters

--- workflow_interactions.r
+++ workflow_interactions.r
@@ -7,50 +7,41 @@
 library(tidyverse)
 library(dplyr)
 
-# data load
-workdir <- "/mnt/atgc-d3/sur/users/azermeno/data/2025-interactions/"
-outdir <- "/mnt/atgc-d3/sur/users/azermeno/exp/2025-interacions/"
+########### Functions #################
 
-meta_mags <- read.csv(file = paste0(workdir, 'metadata.csv'), header = TRUE)
-meta_bgcs <- read.csv(file = paste0(workdir, 'bgcs_metadata.csv'), header = TRUE)
+prep_mags <- function(meta_mags, mags){
+  mags <- enquo(mags)
+  # data prep
+  meta_mags <- meta_mags %>% filter(!is.na(!!mags)) # omit NAs
+  
+  # Count the number of MAGs per site and phylogenetic groups
+  mags_by_sites <- meta_mags %>%
+    count(station, !!mags) %>%
+    pivot_wider(names_from = !!mags, values_from = n, values_fill = 0)
+  
+  return(mags_by_sites)
+}
 
-# data prep
-# We are grouping by mOTUS so we need to omit the NAs / For other categories skip this 
-meta_mags <- meta_mags %>% filter(!is.na(mOTUs.Species.Cluster))
-meta_bgcs <- meta_bgcs %>%
-  filter(Genome %in% meta_mags$Genome)
-
-# number of mOTUs
-#motus <- meta_mags %>% 
-#  distinct(mOTUs.Species.Cluster)
-
-# Count the number of MAGs per site and mOTUs
-motus_by_sites <- meta_mags %>%
-  count(station, mOTUs.Species.Cluster) %>%
-  pivot_wider(names_from = mOTUs.Species.Cluster, values_from = n, values_fill = 0)
-
-# bind the column station to each BGC using the column Genome ass a reference to match it 
-bgcs_sites <- meta_bgcs %>%
-  left_join(meta_mags %>% select(Genome, station), by = "Genome")
-
-# number of GCFs 
-#gcfs <- bgcs_sites %>% 
-#  distinct(gcf)
-
-# count the number of BGCs per site and GCF
-gcfs_by_sites <- bgcs_sites %>%
-  count(station, gcf) %>%
-  pivot_wider(names_from = gcf, values_from = n, values_fill = 0)
+prep_bgcs <- function(meta_bgcs, bgcs, meta_mags){
+  bgcs <- enquo(bgcs)
+  # data prep
+  meta_bgcs <- meta_bgcs %>%             # select only the bgcs present
+    filter(Genome %in% meta_mags$Genome) # in the mags selected ???
+  
+  # count the number of BGCs per site and GCF
+  bgcs_by_sites <- meta_bgcs %>%
+    count(station, !!bgcs) %>%
+    pivot_wider(names_from = !!bgcs, values_from = n, values_fill = 0)
+  
+  return(bgcs_by_sites)
+}  
 
 
-############### Workflow ##############################
-
-# Function
-recreate_table <- function(mag, bgc, motus_by_sites, gcfs_by_sites) {
+recreate_table <- function(mag, bgc, m_by_sites, b_by_sites) {
   
   # select the station column and the current column for both tables
-  table1 <- motus_by_sites[, c("station", mag), drop = FALSE]
-  table2 <- gcfs_by_sites[, c("station", bgc), drop = FALSE]
+  table1 <- m_by_sites[, c("station", mag), drop = FALSE]
+  table2 <- b_by_sites[, c("station", bgc), drop = FALSE]
   
   # bind col1 and col2 by station (full_join porque sino se pierden interacciones)
   table_comb <- full_join(table1, table2, by = "station")
@@ -61,9 +52,25 @@ recreate_table <- function(mag, bgc, motus_by_sites, gcfs_by_sites) {
   
   # Eliminar filas donde ambos valores sean 0
   table_comb <- table_comb[!(table_comb[[mag]] == 0 & table_comb[[bgc]] == 0), ]
-  
+
   return(table_comb)
 }
+
+
+# data load
+workdir <- "/mnt/atgc-d3/sur/users/azermeno/data/2025-interactions/"
+outdir <- "/mnt/atgc-d3/sur/users/azermeno/exp/2025-interacions/"
+
+meta_mags <- read.csv(file = paste0(workdir, 'metadata.csv'), header = TRUE)
+meta_bgcs <- read.csv(file = paste0(workdir, 'bgcs_metadata.csv'), header = TRUE)
+
+
+mags_by_sites <- prep_mags(meta_mags, mOTUs_Species_Cluster)
+bgcs_by_sites<- prep_bgcs(meta_bgcs, gcf, meta_mags)
+
+
+
+############### Workflow ##############################
 
 # tablas donde se van a guardar informacion de los patrones
 co_occur_list <- list()
@@ -72,7 +79,7 @@ co_exclu_list <- list()
 counter <- 0
 start_time <- Sys.time()
 
-for (col1 in colnames(motus_by_sites)) {
+for (col1 in colnames(mags_by_sites)) {
   if (col1 == "station") next
   
   # Imprimir progreso
@@ -86,19 +93,19 @@ for (col1 in colnames(motus_by_sites)) {
     
   }
   
-  for (col2 in colnames(gcfs_by_sites)) {
+  for (col2 in colnames(bgcs_by_sites)) {
     if (col2 == "station") next
     
     # llamar a la funcon
-    temp3 <- recreate_table(col1, col2, motus_by_sites, gcfs_by_sites)
+    temp3 <- recreate_table(col1, col2, mags_by_sites, bgcs_by_sites)
     
     if (nrow(temp3) == 0) next 
     
     # co-ocurrencia
     if (all((temp3[[col1]] > 0) == (temp3[[col2]] > 0))) {
       co_occur_list[[length(co_occur_list) + 1]] <- list(
-        mags = col1, 
-        bgcs = col2, 
+        Mags = col1, 
+        Bgcs = col2, 
         total_sites = nrow(temp3)
       )
     }
@@ -106,8 +113,8 @@ for (col1 in colnames(motus_by_sites)) {
     # co-exclusión
     if (all((temp3[[col1]] == 0 & temp3[[col2]] > 0) | (temp3[[col1]] > 0 & temp3[[col2]] == 0))) {
       co_exclu_list[[length(co_exclu_list) + 1]] <- list(
-        mags = col1, 
-        bgcs = col2, 
+        Mags = col1, 
+        Bgcs = col2, 
         total_sites = nrow(temp3), 
         mag_sites = sum(temp3[[col1]] > 0), 
         bgc_sites = sum(temp3[[col2]] > 0)
