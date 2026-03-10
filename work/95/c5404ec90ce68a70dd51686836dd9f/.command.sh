64bc9bd285b076c66c4e59b8eaa68e50464add6e
Andrea Zermeno Diaz <azermeno@node12.cluster>
2026-03-09 23:55:31 -0600

    Adjusting paths

--- work/95/c5404ec90ce68a70dd51686836dd9f/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-Rscript interactions_MAG-MAG.r         -m mOTUs_Species_Cluster         -t low         -i ../../data/metadata/         -o .
--- work/d3/63b8962c5e13145020352ea9dd0d1c/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-Rscript interactions_MAG-MAG.r         -m mOTUs_Species_Cluster         -t high         -i ../../data/metadata/         -o .
