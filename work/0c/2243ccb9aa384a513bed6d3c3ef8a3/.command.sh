116f2fb5ccd973f3da63b988da99b080b28ca32e
andreaaaz <andyszermeno@gmail.com>
2026-03-09 23:55:02 -0600

    Adjusting paths

---
+++ work/0c/2243ccb9aa384a513bed6d3c3ef8a3/.command.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -ue
+Rscript /mnt/data/sur/users/azermeno/src/MAGs_BGCs_interactions/interactions_MAG-MAG.r         -m mOTUs_Species_Cluster         -t mid         -i ../../data/metadata/         -o .
---
+++ work/67/0b8a77e76986dbe0aef394c42a55e9/.command.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -ue
+Rscript /mnt/data/sur/users/azermeno/src/MAGs_BGCs_interactions/interactions_MAG-BGC.r         -m mOTUs_Species_Cluster         -b gcc         -t mid         -i ../../data/metadata/         -o .
