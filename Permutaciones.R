a3adee258b6939a80d2fcb8dfd976666aec3d6db
andreaaaz <andyszermeno@gmail.com>
2026-05-07 16:45:47 -0600

    Parallelization

--- Permutaciones.R
+++ Permutaciones.R
@@ -16,12 +16,11 @@ library(ggplot2)
 # Args
 option_list <- list(
   make_option(c("-m", "--microbial_lineage"), type="character", default="mOTUs_Species_Cluster", help="Name of the microbial lienage"),
-  make_option(c("-b", "--bgc_groups"), type="character", default="gcf", help="Name of the grou"),
+  make_option(c("-b", "--bgc_groups"), type="character", default="gcc", help="Name of the grou"),
   make_option(c("-s", "--minimum_sites"), type="numeric", default=10, help="Minimum number of sites where a group is present"),
   make_option(c("-t", "--temp"), type="character", default="low", help="Range of temperature (max, mid and min)"),
   make_option(c("-i", "--indir"), type="character", help="Input directory"),
   make_option(c("-o", "--outdir"), type="character", help="Output directory"),
-  make_option(c("-w", "--workdir"), type="character", help="Working directory")
   )
 opt <- parse_args(OptionParser(option_list=option_list))
 
