7086f635959edda65c60d6cb686e1652c30e9eb4
andreaaaz <andyszermeno@gmail.com>
2026-09-08 14:07:03 -0600

    heat map for jaccard index

--- network_QCstats.R
+++ network_QCstats.R
@@ -31,41 +31,41 @@ mb_0_n <-  read.csv("2026-09-interactions/mOTUs_Species_Cluster_gcc/global/nodes
 mmr_0_e <- read.csv("2026-09-interactions/mOTUs_Species_Cluster_gcc/global/edges_mm.csv")
 mmr_0_n <- read.csv("2026-09-interactions/mOTUs_Species_Cluster_gcc/global/nodes_mm.csv")
 
-mm_8_e <- read.csv("2026-09-interactions8/mOTUs_Species_Cluster/global/edges_mm.csv")
-mm_8_n <- read.csv("2026-09-interactions8/mOTUs_Species_Cluster/global/nodes_mm.csv")
-mb_8_e <- read.csv("2026-09-interactions8/mOTUs_Species_Cluster_gcc/global/edges_mb.csv")
-mb_8_n <-  read.csv("2026-09-interactions8/mOTUs_Species_Cluster_gcc/global/nodes_mb.csv")
-mmr_8_e <- read.csv("2026-09-interactions8/mOTUs_Species_Cluster_gcc/global/edges_mm.csv")
-mmr_8_n <- read.csv("2026-09-interactions8/mOTUs_Species_Cluster_gcc/global/nodes_mm.csv")
+mm_08_e <- read.csv("2026-09-interactions8/mOTUs_Species_Cluster/global/edges_mm.csv")
+mm_08_n <- read.csv("2026-09-interactions8/mOTUs_Species_Cluster/global/nodes_mm.csv")
+mb_08_e <- read.csv("2026-09-interactions8/mOTUs_Species_Cluster_gcc/global/edges_mb.csv")
+mb_08_n <-  read.csv("2026-09-interactions8/mOTUs_Species_Cluster_gcc/global/nodes_mb.csv")
+mmr_08_e <- read.csv("2026-09-interactions8/mOTUs_Species_Cluster_gcc/global/edges_mm.csv")
+mmr_08_n <- read.csv("2026-09-interactions8/mOTUs_Species_Cluster_gcc/global/nodes_mm.csv")
 
 # MAKE NETWORKS
 make_network <- function(edges, nodes, directed = FALSE) {
   graph_from_data_frame(d = edges, vertices = nodes, directed = directed)
 }
 g_mm_0  <- make_network(mm_0_e, mm_0_n)
-g_mm_8  <- make_network(mm_8_e, mm_8_n)
+g_mm_08  <- make_network(mm_08_e, mm_08_n)
 g_mm_15 <- make_network(mm_15_e, mm_15_n)
 
 g_mb_0  <- make_network(mb_0_e, mb_0_n)
-g_mb_8  <- make_network(mb_8_e, mb_8_n)
+g_mb_08  <- make_network(mb_08_e, mb_08_n)
 g_mb_15 <- make_network(mb_15_e, mb_15_n)
 
 g_mmr_0  <- make_network(mmr_0_e, mmr_0_n)
-g_mmr_8  <- make_network(mmr_8_e, mmr_8_n)
+g_mmr_08  <- make_network(mmr_08_e, mmr_08_n)
 g_mmr_15 <- make_network(mmr_15_e, mmr_15_n)
 
 # network list
 networks <- list(
   "MAG-MAG_0"  = g_mm_0,
-  "MAG-MAG_8"  = g_mm_8,
+  "MAG-MAG_08"  = g_mm_08,
   "MAG-MAG_15" = g_mm_15,
   
   "MAG-BGC_0"  = g_mb_0,
-  "MAG-BGC_8"  = g_mb_8,
+  "MAG-BGC_08"  = g_mb_08,
   "MAG-BGC_15" = g_mb_15,
   
   "MAG-MAG-rec_0"  = g_mmr_0,
-  "MAG-MAG-rec_8"  = g_mmr_8,
+  "MAG-MAG-rec_08"  = g_mmr_08,
   "MAG-MAG-rec_15" = g_mmr_15
 )
 
@@ -75,7 +75,8 @@ get_network_stats <- function(g) {
   comp <- components(g)
   tibble(n_nodes = vcount(g), 
          n_edges = ecount(g), 
-         density = edge_density(g), 
+         density = edge_density(g),
+         mean_degree = mean(degree(g)),
          diameter = diameter(g, directed = FALSE, weights = NA), # no contar p-values como distancia
          n_components = comp$no, 
          giant_component = max(comp$csize), 
@@ -90,7 +91,6 @@ network_stats <- network_stats %>% # reacomodar
 
 
 # GRAPH
-network_stats$QC <- factor(network_stats$QC, levels = c(0, 8, 15))
 require(gridExtra)
 nodes <- ggplot(network_stats, aes(x = QC, y = n_nodes, color = network_type, group = network_type)) +
   geom_point(size = 2) +
@@ -158,9 +158,42 @@ jaccard_matrix <- function(network_list) {
 mm_networks <- networks[grepl("^MAG-MAG_[0-9]+$", names(networks))]
 mb_networks <- networks[grepl("^MAG-BGC_", names(networks))]
 mmr_networks <- networks[grepl("^MAG-MAG-rec_", names(networks))]
-
 jaccard_mm <- jaccard_matrix(mm_networks)
 jaccard_mb <- jaccard_matrix(mb_networks)
 jaccard_mmr <- jaccard_matrix(mmr_networks)
+
+# graph heatmap
+jaccard_to_df <- function(mat) {    # convestirlo a data frame
+  as.data.frame(mat) %>%
+    rownames_to_column("QC_1") %>%
+    pivot_longer(cols = -QC_1,names_to = "QC_2", values_to = "Jaccard")
+}
+jaccard_mm_df <- jaccard_to_df(jaccard_mm)
+jaccard_mb_df <- jaccard_to_df(jaccard_mb)
+jaccard_mmr_df <- jaccard_to_df(jaccard_mmr)
+
+mag_mag <- ggplot(jaccard_mm_df, aes(x = QC_2, y = QC_1, fill = Jaccard)) +
+  geom_tile() +
+  geom_text( aes(label = round(Jaccard, 2))) +
+  scale_fill_viridis_c(limits = c(0, 1)) +
+  coord_equal() +
+  theme_minimal() +
+  labs(title = "MAG-MAG networks", x = NULL, y = NULL, fill = "Jaccard")
+mag_mag_r <- ggplot(jaccard_mmr_df, aes(x = QC_2, y = QC_1, fill = Jaccard)) +
+  geom_tile() +
+  geom_text( aes(label = round(Jaccard, 2))) +
+  scale_fill_viridis_c(limits = c(0, 1)) +
+  coord_equal() +
+  theme_minimal() +
+  labs(title = "MAG-MAG reconstructed networks", x = NULL, y = NULL, fill = "Jaccard")
+mag_bgc <- ggplot(jaccard_mb_df, aes(x = QC_2, y = QC_1, fill = Jaccard)) +
+  geom_tile() +
+  geom_text( aes(label = round(Jaccard, 2))) +
+  scale_fill_viridis_c(limits = c(0, 1)) +
+  coord_equal() +
+  theme_minimal() +
+  labs(title = "MAG-BGC reconstructed networks", x = NULL, y = NULL, fill = "Jaccard")
+grid.arrange(mag_bgc, mag_mag, mag_mag_r, nrow = 1, ncol = 3)
+
 
 # Node distribution 
