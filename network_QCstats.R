ed4854210151eb4611fb32071ad74c0319d30dfd
andreaaaz <andyszermeno@gmail.com>
2026-09-09 18:46:04 -0600

    Optimizing the data loading

--- network_QCstats.R
+++ network_QCstats.R
@@ -15,115 +15,36 @@ suppressPackageStartupMessages(library(igraph))
 # loading data
 setwd("~/interactions/")
 
-# mm: MAG-MAG, mb: MAG-BGC, mmr: MAG-MAG reconstructed, e: edges, n: nodes, NUMBERS: QC
+qcs <- c("0", "08", "15")
+types <- c("MAG-MAG", "MAG-BGC", "MAG-MAG-rec")
 
-mm_15_e <- read.csv("2026-09-interactions15/mOTUs_Species_Cluster/global/edges_mm.csv")
-mm_15_n <- read.csv("2026-09-interactions15/mOTUs_Species_Cluster/global/nodes_mm.csv")
-mb_15_e <- read.csv("2026-09-interactions15/mOTUs_Species_Cluster_gcc/global/edges_mb.csv")
-mb_15_n <-  read.csv("2026-09-interactions15/mOTUs_Species_Cluster_gcc/global/nodes_mb.csv")
-mmr_15_e <- read.csv("2026-09-interactions15/mOTUs_Species_Cluster_gcc/global/edges_mm.csv")
-mmr_15_n <- read.csv("2026-09-interactions15/mOTUs_Species_Cluster_gcc/global/nodes_mm.csv")
-
-mm_0_e <- read.csv("2026-09-interactions/mOTUs_Species_Cluster/global/edges_mm.csv")
-mm_0_n <- read.csv("2026-09-interactions/mOTUs_Species_Cluster/global/nodes_mm.csv")
-mb_0_e <- read.csv("2026-09-interactions/mOTUs_Species_Cluster_gcc/global/edges_mb.csv")
-mb_0_n <-  read.csv("2026-09-interactions/mOTUs_Species_Cluster_gcc/global/nodes_mb.csv")
-mmr_0_e <- read.csv("2026-09-interactions/mOTUs_Species_Cluster_gcc/global/edges_mm.csv")
-mmr_0_n <- read.csv("2026-09-interactions/mOTUs_Species_Cluster_gcc/global/nodes_mm.csv")
-
-mm_08_e <- read.csv("2026-09-interactions8/mOTUs_Species_Cluster/global/edges_mm.csv")
-mm_08_n <- read.csv("2026-09-interactions8/mOTUs_Species_Cluster/global/nodes_mm.csv")
-mb_08_e <- read.csv("2026-09-interactions8/mOTUs_Species_Cluster_gcc/global/edges_mb.csv")
-mb_08_n <-  read.csv("2026-09-interactions8/mOTUs_Species_Cluster_gcc/global/nodes_mb.csv")
-mmr_08_e <- read.csv("2026-09-interactions8/mOTUs_Species_Cluster_gcc/global/edges_mm.csv")
-mmr_08_n <- read.csv("2026-09-interactions8/mOTUs_Species_Cluster_gcc/global/nodes_mm.csv")
-
-# MAKE NETWORKS
-make_network <- function(edges, nodes, directed = FALSE) {
-  graph_from_data_frame(d = edges, vertices = nodes, directed = directed)
-}
-g_mm_0  <- make_network(mm_0_e, mm_0_n)
-g_mm_08  <- make_network(mm_08_e, mm_08_n)
-g_mm_15 <- make_network(mm_15_e, mm_15_n)
-
-g_mb_0  <- make_network(mb_0_e, mb_0_n)
-g_mb_08  <- make_network(mb_08_e, mb_08_n)
-g_mb_15 <- make_network(mb_15_e, mb_15_n)
-
-g_mmr_0  <- make_network(mmr_0_e, mmr_0_n)
-g_mmr_08  <- make_network(mmr_08_e, mmr_08_n)
-g_mmr_15 <- make_network(mmr_15_e, mmr_15_n)
-
-# network list
-networks <- list(
-  "MAG-MAG_0"  = g_mm_0,
-  "MAG-MAG_08"  = g_mm_08,
-  "MAG-MAG_15" = g_mm_15,
+make_network <- function(qc, type) {
+  prefix <- ifelse(qc == "0", "", qc)
+  dir <- paste0("2026-09-interactions", prefix)
   
-  "MAG-BGC_0"  = g_mb_0,
-  "MAG-BGC_08"  = g_mb_08,
-  "MAG-BGC_15" = g_mb_15,
+  folder <- ifelse(type == "MAG-MAG", "mOTUs_Species_Cluster", "mOTUs_Species_Cluster_gcc")
+  file_type <- ifelse(type == "MAG-BGC", "mb", "mm")
   
-  "MAG-MAG-rec_0"  = g_mmr_0,
-  "MAG-MAG-rec_08"  = g_mmr_08,
-  "MAG-MAG-rec_15" = g_mmr_15
-)
-
-
-# Network Statistics
-get_network_stats <- function(g) {
-  comp <- components(g)
-  tibble(n_nodes = vcount(g), 
-         n_edges = ecount(g), 
-         density = edge_density(g),
-         mean_degree = mean(degree(g)),
-         diameter = diameter(g, directed = FALSE, weights = NA), # no contar p-values como distancia
-         n_components = comp$no, 
-         giant_component = max(comp$csize), 
-         giant_component_prop = max(comp$csize) / vcount(g),
-         transitivity = transitivity(g, type = "global"))
+  edges <- read.csv(file.path(dir, folder, "global", paste0("edges_", file_type, ".csv")))
+  nodes <- read.csv(file.path(dir, folder, "global", paste0("nodes_", file_type, ".csv")))
+  graph_from_data_frame(edges, vertices = nodes, directed = FALSE)
 }
-network_stats <- imap_dfr(networks, ~ get_network_stats(.x) %>%
-                            mutate(network = .y)) %>% 
-  select(network, everything())
-network_stats <- network_stats %>% # reacomodar
-  separate(network, into = c("network_type", "QC"), sep = "_")
+
+networks <- list()
+
+for (type in types) {
+  for (qc in qcs) {
+    networks[[paste0(type, "_", qc)]] <- make_network(qc, type)
+  }
+}
 
 
-# GRAPH
-require(gridExtra)
-nodes <- ggplot(network_stats, aes(x = QC, y = n_nodes, color = network_type, group = network_type)) +
-  geom_point(size = 2) +
-  geom_line() +
-  theme_minimal()
-edges <- ggplot(network_stats, aes(x = QC, y = n_edges, color = network_type, group = network_type)) +
-  geom_point(size = 2) +
-  geom_line() +
-  theme_minimal()
-density <- ggplot(network_stats, aes(x = QC, y = density, color = network_type, group = network_type)) +
-  geom_point(size = 2) +
-  geom_line() +
-  theme_minimal()
-diameter <- ggplot(network_stats, aes(x = QC, y = diameter, color = network_type, group = network_type)) +
-  geom_point(size = 2) +
-  geom_line() +
-  theme_minimal()
-degree <- ggplot(network_stats, aes(x = QC, y = mean_degree, color = network_type, group = network_type)) +
-  geom_point(size = 2) +
-  geom_line() +
-  theme_minimal()
-component <- ggplot(network_stats, aes(x = QC, y = giant_component, color = network_type, group = network_type)) +
-  geom_point(size = 2) +
-  geom_line() +
-  theme_minimal()
-transitivity <- ggplot(network_stats, aes(x = QC, y = transitivity, color = network_type, group = network_type)) +
-  geom_point(size = 2) +
-  geom_line() +
-  theme_minimal()
-grid.arrange(nodes, edges, density, diameter, degree, component, transitivity)
+# Node statistics distribution
 
-# compare networks with Jaccard
 
+
+
+# JACCARD
 # calcular el indice de jaccard de aristas entre dos redes
 jaccard_edges <- function(g1, g2) {
   edges1 <- apply(as_edgelist(g1), 1, function(x) {
@@ -196,4 +117,6 @@ mag_bgc <- ggplot(jaccard_mb_df, aes(x = QC_2, y = QC_1, fill = Jaccard)) +
 grid.arrange(mag_bgc, mag_mag, mag_mag_r, nrow = 1, ncol = 3)
 
 
-# Node distribution 
+# Shared edges between networks
+
+
